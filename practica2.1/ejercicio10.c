dd48db76a3aa37340ade499ca153965d03376f15
marcos <mamtute@ucm.es>
2021-10-24 19:49:25 +0200

    Ejercicio 10

---
+++ practica2.1/ejercicio10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <pwd.h>
+#include <sys/utsname.h>
+#include <sys/types.h>
+
+int main(){
+	printf("Real: %d\n", getuid());
+	printf("Efectivo: %d\n", geteuid());
+	
+	struct passwd *p = getpwuid(getuid());
+	printf("Nombre de usuario: %s\n", p->pw_name);
+	printf("Directorio home: %s\n", p->pw_dir);
+	printf("Descripción del usuario: %s\n", p->pw_gecos);
+	
+	return 1;
+}
