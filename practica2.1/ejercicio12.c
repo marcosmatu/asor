57a1c27543bb578334aeabdc01b0cc30bb4bf1af
marcos <mamtute@ucm.es>
2021-10-24 20:00:57 +0200

    Ejercicio 12

---
+++ practica2.1/ejercicio12.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+
+int main(){
+	time_t t = time(NULL);
+	printf("Hora desde el Epoch en segundos: %ld\n", t);
+	return 1;
+}
