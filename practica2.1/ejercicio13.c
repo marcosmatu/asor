1e5583ff3d9af7cadd8d2a8fa135d12a31528443
marcos <mamtute@ucm.es>
2021-10-24 20:10:30 +0200

    Ejercicio 13

---
+++ practica2.1/ejercicio13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/time.h>
+
+int main(){
+	struct timeval t1, t2;
+	gettimeofday(&t1,NULL);
+	
+	int cont = 0;
+	for(int i = 0; i < 1000000;i++){
+		cont++;
+	}
+	
+	gettimeofday(&t2,NULL);
+	printf("Microsegundos que tarda en incrementar un millón de veces una variable: %ld\n", t2.tv_usec - t1.tv_usec);
+	return 1;
+}
