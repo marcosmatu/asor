acd363d90c9b3cbd125b10d630a1274afece4bc3
marcos <mamtute@ucm.es>
2021-10-24 19:33:52 +0200

    Ejercicio9

---
+++ practica2.1/ejercicio9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/utsname.h>
+#include <sys/types.h>
+
+int main(){
+	printf("Real: %d\n", getuid());
+	printf("Efectivo: %d\n", geteuid());
+	return 1;
+}
