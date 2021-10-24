0f5a305829886954e8b56301112a5a369d20505e
marcos <mamtute@ucm.es>
2021-10-24 18:41:40 +0200

    Ejercicio 5. -Falla domain name.

---
+++ practica2.1/ejercicio5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/utsname.h>
+#include <sys/types.h>
+
+int main(){
+	struct utsname buffer;
+	if(uname(&buffer)==-1){
+		printf("Error %d: %s\n", errno, strerror(errno));
+		return -1;
+	}else{
+		printf("sysname: %s\n", buffer.sysname);
+		printf("nodename; %s\n", buffer.nodename);
+		printf("release: %s\n", buffer.release);
+		printf("version: %s\n", buffer.version);
+		printf("machine: %s\n", buffer.machine);
+		printf("domain name: %s\n", buffer.__domainname);
+		return 1;
+	}
+}
