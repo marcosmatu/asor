8b0060614d52ad8b3345e22ad329533860ec2a07
marcos <mamtute@ucm.es>
2021-10-25 10:09:44 +0200

    Ejercicio 14

---
+++ practica2.1/ejercicio14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/time.h>
+
+int main(){
+	time_t t = time(NULL);
+	struct tm *time = localtime(&t);
+	printf("Estamos en el año: %d\n",time->tm_year + 1900);
+	return 1;
+} 
