b64ba2fc2b905c3eac033ddbedd673d034b7b0fb
marcos <mamtute@ucm.es>
2021-10-29 14:58:19 +0200

    Ejercicio2 final.

--- practica2.1/ejercicio2.c
+++ practica2.1/ejercicio2.c
@@ -4,6 +4,9 @@
 #include <errno.h>
 
 int main(){
-	if(setuid(0) == -1) printf("Error %d: %s\n", errno,strerror(errno));
-	return 1;
+	if(setuid(0) == -1) {
+		printf("Error %d: %s\n", errno,strerror(errno));
+		return -1;
+	}
+	return 0;
 }
