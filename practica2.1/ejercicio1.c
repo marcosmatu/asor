983cb03b4eb447a2d83618ecc1cd8fbc7d3fc09d
marcos <mamtute@ucm.es>
2021-10-29 12:31:07 +0200

    Ejercicio3

--- practica2.1/ejercicio1.c
+++ practica2.1/ejercicio1.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
-
 int main(){
 	if(setuid(0) == -1) perror("Error setuid 2");
 	return 1;
--- practica2.1/ejercicio3.c
+++ practica2.1/ejercicio3.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 int main(){
-	for(int i = 1; i <= 255; i++){
+	int i;
+	for(i = 1; i <= 255; i++){
 		printf("Error %i: %s\n",i,strerror(i));
 	}
 	return 1;
