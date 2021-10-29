166a4798b6bc97cea3e39bfff045a772b11ad4cf
marcos <mamtute@ucm.es>
2021-10-29 15:21:34 +0200

    Ejercicio 5 final.

--- practica2.1/ejercicio5.c
+++ practica2.1/ejercicio5.c
@@ -11,13 +11,17 @@ int main(){
 	if(uname(&buffer)==-1){
 		printf("Error %d: %s\n", errno, strerror(errno));
 		return -1;
-	}else{
-		printf("sysname: %s\n", buffer.sysname);
-		printf("nodename; %s\n", buffer.nodename);
-		printf("release: %s\n", buffer.release);
-		printf("version: %s\n", buffer.version);
-		printf("machine: %s\n", buffer.machine);
-		printf("domain name: %s\n", buffer.__domainname);
-		return 1;
 	}
+	printf("sysname: %s\n", buffer.sysname);
+	printf("nodename: %s\n", buffer.nodename);
+	printf("release: %s\n", buffer.release);
+	printf("version: %s\n", buffer.version);
+	printf("machine: %s\n", buffer.machine);
+	printf("domain name: %s\n", buffer.__domainname);
+	/*
+	#ifdef _GNU_SOURCE
+		printf("domain name: %s\n", buffer.domainname);
+	#endif
+	*/
+	return 0;
 }
