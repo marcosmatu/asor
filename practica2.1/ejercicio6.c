30f973e539efc1b3f048b3ce3f52aadddfa386b6
marcos <mamtute@ucm.es>
2021-10-30 19:18:56 +0200

    Ejercicio 6 final.

--- practica2.1/ejercicio6.c
+++ practica2.1/ejercicio6.c
@@ -8,9 +8,9 @@
 
 int main(){
 	long int arg,hij,fichero;
-	if((arg = sysconf(_SC_ARG_MAX) == -1) || (hij = sysconf(_SC_CHILD_MAX) == -1) || (fichero = sysconf(_SC_OPEN_MAX)==-1)) return -1;
-	printf("Longitud máxima de los argumentos: %ld\n", sysconf(_SC_ARG_MAX));
-	printf("Número máximo de hijos: %ld\n", sysconf(_SC_CHILD_MAX));
-	printf("Número máximo de ficheros: %ld\n", sysconf(_SC_OPEN_MAX));
+	if((arg = sysconf(_SC_ARG_MAX)) == -1 || (hij = sysconf(_SC_CHILD_MAX)) == -1 || (fichero = sysconf(_SC_OPEN_MAX)) == -1) return -1;
+	printf("Longitud máxima de los argumentos: %ld\n", arg);
+	printf("Número máximo de hijos: %ld\n", hij);
+	printf("Número máximo de ficheros: %ld\n", fichero);
 	return 0;
 }
