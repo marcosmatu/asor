be397beac3f0802880aa953e450354f58e9e7db6
marcos <mamtute@ucm.es>
2021-10-30 17:15:07 +0200

    Ejercicio 7 final.

--- practica2.1/ejercicio7.c
+++ practica2.1/ejercicio7.c
@@ -7,8 +7,10 @@
 #include <sys/types.h>
 
 int main(){
+	long int en,rut,nom;
+	if((en = pathconf("/", _PC_LINK_MAX) == -1) || (rut = pathconf("/",_PC_PATH_MAX) == -1) || (nom = pathconf("/",_PC_NAME_MAX) == -1)) return -1;
 	printf("Número máximo de enlaces: %ld\n", pathconf("/", _PC_LINK_MAX));
 	printf("Tamaño máximo de una ruta: %ld\n", pathconf("/",_PC_PATH_MAX));
 	printf("Tamaño máximo de un nombre de fichero: %ld\n", pathconf("/",_PC_NAME_MAX));
-	return 1;
+	return 0;
 }
