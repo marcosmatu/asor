85cd35cd4178f3215f7ae9f6957b2c9575998cd0
marcos <mamtute@ucm.es>
2021-11-11 18:49:52 +0100

    Ejercicio 5 y 7.

--- practica2.2/ejercicio5.c
+++ practica2.2/ejercicio5.c
@@ -1,9 +1,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(){
 	int fich = open("ej5", O_CREAT,0645);
 	if(fich == -1) return -1;
+	close(fich);
 	return 0;
 }
--- practica2.2/ejercicio7.c
+++ practica2.2/ejercicio7.c
@@ -1,10 +1,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(){
 	umask(026);
 	int fich = open("ej7", O_CREAT,0540);
 	if(fich == -1) return -1;
+	close(fich);
 	return 0;
 }
